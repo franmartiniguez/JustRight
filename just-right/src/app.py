2116be8570ffec1b1d28775b26ecf0ddd7910285
franmartiniguez <franmartiniguez@gmail.com>
2023-11-26 17:44:18 -0800

    finished firebase functionality and flask backend

---
+++ just-right/src/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, jsonify, redirect
+from firebase_admin import credentials, firestore
+from firebase import Firebase  # Import your Firebase class from the previous code
+
+app = Flask(__name__)
+
+# Initialize Firebase
+firebase_instance = Firebase()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add_rental', methods=['POST'])
+def add_rental():
+    if request.method == 'POST':
+        # Retrieve data from the form
+        address = request.form['address']
+        landlord_rating = float(request.form['landlord_rating'])
+        landlord_review = request.form['landlord_review']
+        price = float(request.form['price'])
+        rental_rating = float(request.form['rental_rating'])
+        rental_review = request.form['rental_review']
+
+        # Add the rental to the database
+        firebase_instance.add_rental(address, landlord_rating, landlord_review, price, rental_rating, rental_review)
+
+        # Redirect to the home page or another appropriate page
+        return redirect('/')
+    else:
+        return 'Invalid request method'
+
+@app.route('/get_all_rentals', methods=['GET'])
+def get_all_rentals():
+    order_by = request.args.get('order_by')
+    descending = request.args.get('descending') == 'True'
+
+    # Get all rentals based on user preferences
+    rentals = firebase_instance.get_all_rentals(order_by=order_by, descending=descending)
+
+    return jsonify(rentals)
+
+if __name__ == '__main__':
+    app.run()
